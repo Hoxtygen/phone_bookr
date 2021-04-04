317d615e86d808a606d895ee38e1926c7a430f31
Wasiu Idowu <hoxtygen@live.com>
2021-04-04 14:23:38 +0100

    Abstract styling to a different file

---
+++ lib/constants/style_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kcontactTileTitleStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 23.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kcontactTileSubtitleStyle = TextStyle(
+  color: Colors.black26,
+  fontSize: 18.0,
+  fontWeight: FontWeight.w700,
+);
