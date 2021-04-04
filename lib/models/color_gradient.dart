0902d385cb014b484c81f74c2f9d027b0a19b898
Wasiu Idowu <hoxtygen@live.com>
2021-04-04 14:27:09 +0100

    Added color gradients for random color generation

--- lib/models/color_gradient.dart
+++ lib/models/color_gradient.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 LinearGradient colorGradient(Color color) {
   var baseColor = color as dynamic;
-  Color color1 = baseColor[800];
-  Color color2 = baseColor[400];
+  Color colorA = baseColor[800];
+  Color colorB = baseColor[200];
+
   return LinearGradient(colors: [
-    color1,
-    color2,
+    colorA,
+    colorB,
   ], begin: Alignment.bottomLeft, end: Alignment.topRight);
 }
